2b9381f4839be77bf73f14d710487e54ab64c804
ssnigdha05 <53087418+ssnigdha05@users.noreply.github.com>
2019-07-19 09:41:50 -0400

    Add files via upload

---
+++ jweb.js
@@ -0,0 +1,11 @@
+alert("Welcome to my page!")
+
+function bigImg(x) {
+  x.style.height = "400px";
+  x.style.width = "400px";
+}
+
+function smallImg(x) {
+  x.style.height = "160px";
+  x.style.width = "300px";
+}
